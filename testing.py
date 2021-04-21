1664b43aabe80ef3718fdc61a43f3874bd32ad8b
Am271 <>
2021-04-21 09:09:02 +0000

    demonstrated sliding mechanism

--- testing.py
+++ testing.py
@@ -1,5 +1,16 @@
+import timeit
+
 def slide(buf, ch):
     #buf is the buffer that will be slided, ch is the character that will be added at the last position after sliding
     for i in range(len(buf)-1):
         buf[i] = buf[i + 1]
     buf[len(buf) - 1] = ch
+
+# search buffer = xxxxoof character added is b
+teststr = 'xxxxoof'
+testbuf = [i for i in teststr]
+# print('{} takes {} usec/loop'.format(slide.__name__, timeit.timeit(slide(testbuf, 'b'), number=1000)*1000))
+
+print(testbuf) #before sliding
+slide(testbuf, 'b') #sliding, b is the character that will be added to the last position
+print(testbuf) #after sliding
