b5cd6731a07d3f96ee7a48c2da9c801f45f8393e
Amitash M S <amitu895@gmail.com>
2021-04-21 23:32:07 +0530

    compare function added for encoding - v1

--- testing.py
+++ testing.py
@@ -1,5 +1,3 @@
-import timeit
-
 def slide(buf, ch):
     #buf is the buffer that will be slided, ch is the character that will be added at the last position after sliding
     for i in range(len(buf)-1):
@@ -14,3 +12,30 @@ testbuf = [i for i in teststr]
 print(testbuf) #before sliding
 slide(testbuf, 'b') #sliding, b is the character that will be added to the last position
 print(testbuf) #after sliding
+
+def compare(sbuf, labuf, j):
+    if labuf[0] in sbuf and j > -1 and j < len(sbuf):
+        offsets = []; matches = []; tmp = []; ttmp = []
+        flag = 0
+        for i in range(len(labuf)):
+            while j > -1 and j < len(sbuf):
+                if sbuf[j] == labuf[i]:
+                    offsets.append(len(sbuf) - j)
+                    tmp.append(sbuf[j])
+                    flag = 1
+                    j += 1; break
+                else:
+                    j -= 1
+        if len(tmp) > 0:
+            ttmp = compare(sbuf, labuf, len(sbuf) - max(offsets) - 1)
+        if len(ttmp) > 0:
+            return ttmp
+        else:
+            return tmp
+    else:
+        return []
+
+sbuf = [i for i in 'xofgxof']
+labuf = [i for i in 'ofghxx']
+
+print(compare(sbuf, labuf, len(sbuf) - 1))
