1234677a04251c5ac75c6c59e08f1e678379b337
Amitash M S <amitu895@gmail.com>
2021-04-23 00:20:36 +0530

    fixed small bug, working - v3

--- common.py
+++ common.py
@@ -6,6 +6,7 @@ def LCSubStr(X, Y, m, n):
 	result = 0
 	length = 0
 	common = []
+	offset = 0
 
 	if(X[0] in Y):
 		for i in range(m + 1):
@@ -17,38 +18,45 @@ def LCSubStr(X, Y, m, n):
 					result = max(result, LCSS[i][j])
 				else:
 					LCSS[i][j] = 0
-		
+		# print(LCSS)
 		for i in range(n + 1):
 			if LCSS[1][i] ==  1 and not length:
 				length = 1
 				common.append(Y[i-1])
+				offset = i
 				continue
-			if length :
+			if length:
 				try:
-					if LCSS[length+1][i]:
+					if LCSS[length+1][i] and LCSS[length][i-1]:
 						length += 1
 						common.append(Y[i-1])
+						offset = i
 				except:
 					pass
-		if Y[len(Y)-length] == X[length]:
-			length += LCSubStr(Y[len(Y)-length:len(Y)],X[length:len(X)],len(Y[len(Y)-length:len(Y)]),len(X[length:len(X)]))
 			
-	return length
+		# print(length)
+		tY = Y[offset - 1:offset - 1 + length]
+		tX = X[length:]
+		if tY[0] == X[length]:
+			length += LCSubStr(tX, tY, len(tX), len(tY))
+			
+	return offset, length
 
 
 # Driver Code
-X = 'rarrad'     #lab
-Y = 'adabrar'   #sb
+# X = 'rarrad'     #lab
+# Y = 'adabrar'   #sb
+
+X = 'ofghxx'	#lab
+Y = 'xxxoxxf'	#sb
+
+# X = 'bbcxdf'
+# Y = 'asfbbac'
 
 X1 = [i for i in X]
 Y1 = [j for j in Y]
-# X = 'ofghxx'    lab
-# Y = 'xxxxoof'   sb
-
-
 
 m = len(X1)
 n = len(Y1)
 
-print('Length of Longest Common Substring is',
-	LCSubStr(X1, Y1, m, n))
+print(LCSubStr(X1, Y1, m, n))
